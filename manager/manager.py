e70d43ebf4ed4ea4034135e9de65e935ebd74edd
GoNorman <dr.cheif-afanasyev@yandex.ru>
2023-10-04 17:30:54 +0200

    modefied manager

--- manager/manager.py
+++ manager/manager.py
@@ -12,6 +12,21 @@ class Manager:
         list_resource.append(resource)
         return list_resource.index(resource)
     
+    def create_resource_bd(request, conn):
+        data = request.get_json()
+        name = data.get('name')
+        current_speed = data.get('current_speed')
+        type_id = data.get('type_id')
+
+        cursor = conn.cursor()
+        cursor.execute(
+        "INSERT INTO resource (name, current_speed, type_id) VALUES (%s, %s, %s)",
+        (name, current_speed, type_id)
+        )
+        conn.commit()
+
+        return {"message": "Resource created successfully"}
+    
     def change_resource(id, resource):
         list_resource[id] = resource
     
@@ -40,4 +55,22 @@ class Manager:
         }
         print(json.dumps(json_x))
         return json.dumps(json_x)
+    
+    def get_all_resources(request, conn):
+        cursor = conn.cursor()
+        cursor.execute(
+        "SELECT r.id, r.name, r.current_speed, rt.name as type_name, rt.max_speed FROM resource r INNER JOIN resource_type rt ON r.type_id = rt.id"
+        )
+        resources = cursor.fetchall()
+        result = []
+        for row in resources:
+            resource_data = {
+                "id": row[0],
+                "name": row[1],
+                "current_speed": row[2],
+                "type_name": row[3],
+                "max_speed": row[4]
+            }
+            result.append(resource_data)
+        return {"resources": result}
 
